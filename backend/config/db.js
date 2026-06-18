a9edbf4dc5b757f7835c67821edce15f147a5800
Shitansh <shitanshwankhede@gmail.com>
2026-06-18 23:55:18 +0530

    dns.setServers([8.8.8.8, 1.1.1.1]); since it dns causing backend start error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database connected successfully");
-    }
-    catch(error){
-        console.log("Database didn't connect successfully");
-        console.error(error);
-        process.exit(1);
-    }
-}
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database didn't connect successfully");
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
